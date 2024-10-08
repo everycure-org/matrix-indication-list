bbebc635ebf346becdbb8dc0fbef901eb1dd6bdf
Marcello DeLuca <marcello-deluca@Marcellos-MacBook-Air.local>
2024-10-08 11:46:47 -0400

    diseases to IDs

--- contraindications/contraindications_to_diseases/diseaseList_to_ids/diseases_to_ids.py
+++ contraindications/contraindications_to_diseases/diseaseList_to_ids/diseases_to_ids.py
@@ -7,19 +7,20 @@ import asyncio
 import aiohttp
 from tenacity import retry, stop_after_attempt, wait_exponential
 from ratelimit import limits
+import time
+#from joblib import Memory
+#memory = Memory(location=".cache/nameres", verbose=0)
 
-@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10))
-#@limits(calls=500, period=1)
-async def getCurie_Disease_async(session, name, disease_id_label_list, index, cache):
+@retry(stop=stop_after_attempt(10), wait=wait_exponential(multiplier=1, min=4, max=10))
+async def getCurie_async(session, name, disease_id_label_list, index, cache, biolink_class):
     if name in cache:
         disease_id_label_list[index] = cache[name]
-        return
     else:
-        itemRequest = f'https://name-resolution-sri.renci.org/lookup?string={name}&autocomplete=false&offset=0&limit=10&biolink_type=DiseaseOrPhenotypicFeature'
+        itemRequest = f'https://name-resolution-sri.renci.org/lookup?string={name}&autocomplete=false&offset=0&limit=10&biolink_type={biolink_class}'
         async with session.get(itemRequest) as response:
-            text = await response.text()
+            out_json = await response.json()
         try:
-            returned = pd.read_json(StringIO(text))
+            returned = pd.DataFrame.from_dict(out_json)
             resolvedName = returned.curie
             resolvedLabel = returned.label
             disease_id_label_list[index] = resolvedName[0], resolvedLabel[0]
@@ -30,30 +31,7 @@ async def getCurie_Disease_async(session, name, disease_id_label_list, index, ca
         except Exception as e:
             print(f"Unexpected error processing {name}: {str(e)}")
             disease_id_label_list[index] = "Error", "Unexpected Error"
-
-@retry(stop=stop_after_attempt(3), wait=wait_exponential(multiplier=1, min=4, max=10))
-#@limits(calls=500, period=1)
-async def getCurie_Drug_async(session, name, drug_id_label_list, index, cache):
-    if name in cache:
-        #print(f"cache hit for {name}")
-        drug_id_label_list[index] = cache[name]
-        return
-    else:
-        itemRequest = f'https://name-resolution-sri.renci.org/lookup?string={name}&autocomplete=false&offset=0&limit=10&biolink_type=ChemicalOrDrugOrTreatment'
-        async with session.get(itemRequest) as response:
-            text = await response.text()
-        try:
-            returned = pd.read_json(StringIO(text))
-            resolvedName = returned.curie
-            resolvedLabel = returned.label
-            drug_id_label_list[index] = resolvedName[0], resolvedLabel[0]
-            cache.update({name: (resolvedName[0], resolvedLabel[0])})
-        except (asyncio.TimeoutError, aiohttp.ClientError, pd.errors.EmptyDataError) as e:
-            print(f"Error processing {name}: {str(e)}")
-            drug_id_label_list[index] = "Error", str(e)
-        except Exception as e:
-            print(f"Unexpected error processing {name}: {str(e)}")
-            drug_id_label_list[index] = "Error", "Unexpected Error"
+            print(f"Response:{response}")
 
 def build_string_from_list(list):
     outString = "["
@@ -62,85 +40,47 @@ def build_string_from_list(list):
     outString = outString[:-2] + "]"
     return outString
 
-async def getAllDiseaseCuries(names: list[str], disease_ids_preallocated):
+async def getAllCuries(names: list[str], ids_preallocated, biolink_type):
     cache = {}
     async with aiohttp.ClientSession() as session:
-        tasks = [getCurie_Disease_async(session, name, disease_ids_preallocated, idx, cache) for idx, name in enumerate(names)]
-        print("Resolving disease IDs and labels...")
+        tasks = [getCurie_async(session, name, ids_preallocated, idx, cache, biolink_type) for idx, name in enumerate(names)]
+        print("Resolving IDs and labels...")
         for f in tqdm.asyncio.tqdm.as_completed(tasks):
             await f
-
-async def getAllDrugCuries(names: list[str], drug_ids_preallocated):
-    cache = {}
-    async with aiohttp.ClientSession() as session:
-        tasks = [getCurie_Drug_async(session, name, drug_ids_preallocated, idx, cache) for idx, name in enumerate(names)]
-        print("Resolving drug IDs and labels...")
-        for f in tqdm.asyncio.tqdm.as_completed(tasks):
-            try:
-                await f
-            except:
-                print("exception at await task level")
         
 async def main():
     diseaseData = pd.read_excel('../active_ingredients_to_structured_lists_v2.xlsx')
     diseaseList = []
     drugList = []
     source_list = []
-    
-    nRows = len(list(diseaseData['Structured Disease list']))
-    
-    labelDict = {}
-    idDict = {}
-        
-    
+
     print("creating tasks")
     n_sections = len(diseaseData)
+    index_min = 0
     #limit = n_sections
-    limit = 10000
+    limit = 5000
     for index, row in tqdm.tqdm(diseaseData.iterrows(), total=n_sections):
         drug = row['Active Ingredients']
         diseases = row['Structured Disease list']
-        if index < limit:
-            curr_row_diseasesTreated = row['Structured Disease list']
-            
+        if index >= index_min and index < limit:
+            curr_row_diseasesTreated = row['Structured Disease list']    
             if type(curr_row_diseasesTreated)!=float:
-                curr_row_drugsInTherapy = row['Active Ingredients']
-                curr_row_disease_ids = []
-                curr_row_disease_id_labels = []
                 curr_row_diseaseList = curr_row_diseasesTreated.replace("[","").replace("]","").replace('\'','').split(',')
-        
                 for idx2,item in enumerate(curr_row_diseaseList):             
                     item = item.strip().upper().replace(" \n","").replace(" (PREVENTATIVE)","")
                     diseaseList.append(item)
                     drugList.append(drug)
                     source_list.append(diseases)
 
-    print(len(diseaseList), " items in disease list")
-    print(f"Correspondingly, {len(drugList)} sets of active ingredients")
-
     disease_ids_and_labels = [("","")]*len(diseaseList)
     drug_ids_and_labels = [("","")]*len(diseaseList)
-
-    await getAllDiseaseCuries(diseaseList, disease_ids_and_labels)
-    await getAllDrugCuries(drugList, drug_ids_and_labels)
-
-    print(disease_ids_and_labels)
-    print(drug_ids_and_labels)
-
-    print(type(disease_ids_and_labels))
-    print(type(drug_ids_and_labels))
-
+    print("Resolving disease IDs")
+    await getAllCuries(diseaseList, disease_ids_and_labels, 'DiseaseOrPhenotypicFeature')
+    
+    print("Resolving drug IDs")
+    await getAllCuries(drugList, drug_ids_and_labels, 'ChemicalOrDrugOrTreatment')
     diseaseIDList, diseaseLabelList = zip(*disease_ids_and_labels)
     drugIDList, drugLabelList = zip(*drug_ids_and_labels)
-    
-    print(len(drugList))
-    print(len(drugIDList))
-    print(len(drugLabelList))
-    print(len(diseaseList))
-    print(len(diseaseIDList))
-    print(len(diseaseLabelList))
-    print(len(source_list))
-
 
     data = pd.DataFrame({
         "active ingredients": drugList,
@@ -151,7 +91,6 @@ async def main():
         "disease label": diseaseLabelList,
         "source list": source_list
     })
-
     data.to_excel("contraindicationList.xlsx")
 
 asyncio.run(main())
